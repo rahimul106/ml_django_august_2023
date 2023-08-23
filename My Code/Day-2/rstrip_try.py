9b36bec4cc7d3287c285fcb06881803c144af991
rahimul.islam <rahimul.islam@ibcs-primax.com>
2023-08-23 23:20:06 +0600

    rstrip() practice

---
+++ My Code/Day-2/rstrip_try.py
@@ -0,0 +1,11 @@
+demo5 = '     Bangladesh     '
+print('My country is',demo5,'.')
+
+rm_space5 = demo5.rstrip()
+print('My country is',rm_space5,'.')
+
+demo6 = ',,,,,,,,,,gggggyyytt ...    Bangladeshy..ggg...yyttt'
+print('My country is',demo6,'.')
+
+rm_space6= demo6.rstrip(',.gyt ')
+print('My country is',rm_space6,'.')
