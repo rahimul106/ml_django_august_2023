5ff9c0274c923f3652d44fd580870f978134ffc3
rahimul.islam <rahimul.islam@ibcs-primax.com>
2023-08-23 22:55:44 +0600

    strip() practice

---
+++ My Code/Day-2/strip_try.py
@@ -0,0 +1,17 @@
+
+# #Write  a sentence leading and trailing whitespaces
+# demo1 = '     Bangladesh     '
+# print('My country is',demo1,'.')
+
+# #Remove the leading and trailing whitespaces 
+# rm_space1= demo1.strip()
+# print('My country is',rm_space1,'.')
+
+#Write  another sentence
+demo2 = ',,,,,,,,,,gggggyyytt ...    Bangladeshy.....yyttt'
+print('My country is',demo2,'.')
+
+# #Remove the leading and trailing some charater untill come another character
+rm_space2= demo2.strip(',.gyt ')
+print('My country is',rm_space2,'.')
+
