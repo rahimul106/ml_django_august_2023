0cc7ef49d4bed00984659df98f1da04dd846bae5
rahimul.islam <rahimul.islam@ibcs-primax.com>
2023-08-25 01:13:35 +0600

    replace item practice in python

---
+++ My Code/Day-3/replace_try.py
@@ -0,0 +1,17 @@
+#Given the list animals = ['dog', 'cat', 'elephant', 'lion'], how can you replace 'elephant' with 'tiger'?
+#List 
+animals = ['dog', 'cat', 'elephant', 'lion']
+print(animals)
+
+#Replace 'elephant' with 'tiger'
+#Find index of elephant
+index_ele = animals.index('elephant')
+print(index_ele)
+
+#Remove 'elephant' from the list
+animals.pop(index_ele)
+print(animals)
+
+#Now insert 'tiger' same index of 'elephant'
+animals.insert(2,'tiger')
+print(animals)
