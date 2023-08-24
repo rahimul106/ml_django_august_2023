6668ab83922f70b785babd37176ce2cbbd1e54bb
rahimul.islam <rahimul.islam@ibcs-primax.com>
2023-08-24 23:35:19 +0600

    count method practice in python

---
+++ My Code/Day-3/count_try.py
@@ -0,0 +1,7 @@
+#If you have the list letters = ['a', 'b', 'c', 'a', 'd'], how many occurrences of the letter 'a' are there in the list?
+#List
+letters = ['a', 'b', 'c', 'a', 'd']
+print(letters)
+
+#Find out how many occurrences of the letter 'a' are there in the list?
+print('Occurrences of the letter a are there in the list are :',letters.count('a'))
