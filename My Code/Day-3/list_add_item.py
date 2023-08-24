9008493d7de294f71e7663b478149efade761edb
rahimul.islam <rahimul.islam@ibcs-primax.com>
2023-08-24 23:24:52 +0600

    remove item practice in python

---
+++ My Code/Day-3/list_add_item.py
@@ -0,0 +1,13 @@
+#Given the list fruits = ['apple', 'banana', 'cherry'], how can you add the item 'orange' to the end of the list?
+#List
+fruits = ['apple','banana','cherry']
+print(fruits)
+# print(type(fruits)) find type
+#print(len(fruits)) find lenth
+
+#add a item to the end of this list
+fruits.append('orange')
+
+#check out ornge add or not add end of the list
+print(fruits)
+print(len(fruits))
---
+++ My Code/Day-3/remove_item.py
@@ -0,0 +1,14 @@
+#If you have the list numbers = [1, 2, 3, 4, 5], what method would you use to remove the item 3 from the list?
+#List 
+numbers = [1,2,3,4,5]
+print(numbers)
+
+# find type
+#print(type(numbers)) 
+
+# find lenth
+#print(len(numbers)) 
+
+#remove the item 3 from the list
+numbers.remove(3)
+print(numbers)
