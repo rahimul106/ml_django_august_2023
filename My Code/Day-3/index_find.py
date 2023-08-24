b87128bdc1d4acd3a88c02142e6617d6cf90983f
rahimul.islam <rahimul.islam@ibcs-primax.com>
2023-08-24 23:55:54 +0600

    Index Find from List practice in python

---
+++ My Code/Day-3/index_find.py
@@ -0,0 +1,7 @@
+#You have the list cities = ['New York', 'Los Angeles', 'Chicago', 'Houston']. How can you find the index of the item 'Chicago'?
+#List
+cities = ['New York', 'Los Angeles', 'Chicago', 'Houston']
+print(cities)
+
+#Find index of item 'Chicago' from the list
+print('Find index of item Chicago from the list is -',cities.index('Chicago'))
