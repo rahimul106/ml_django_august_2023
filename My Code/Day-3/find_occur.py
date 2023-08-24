4b420e462ee2313ac57f9da219b5aa89fe79f9a3
rahimul.islam <rahimul.islam@ibcs-primax.com>
2023-08-25 02:23:50 +0600

    Prove that using count method to count the number of occurrences in a list in python

---
+++ My Code/Day-3/find_occur.py
@@ -0,0 +1,12 @@
+#If you have the list numbers = [5, 2, 8, 2, 1], what method can you use to count the number of occurrences of the value 2?
+#I use count method
+#Here the given list 2 have two occurrences
+#Let's use count method and see output
+
+#Given List
+numbers = [5, 2, 8, 2, 1]
+
+#Use count method find occurrences
+occurrence = numbers.count(2)
+print(occurrence)
+#Output is 2 , so I succsseed to find occurrences 2 in this list using count method
