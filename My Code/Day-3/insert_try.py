1e7d501ca666e0819d1d6e747e38e88e38a275bd
rahimul.islam <rahimul.islam@ibcs-primax.com>
2023-08-24 23:30:14 +0600

    insert item practice in python

---
+++ My Code/Day-3/insert_try.py
@@ -0,0 +1,8 @@
+#You have the list colors = ['red', 'green', 'blue']. How can you insert the item 'yellow' at the second position in the list?
+#List
+colors = ['red', 'green', 'blue']
+print(colors)
+
+#Inser a item in 2nd position in the list
+colors.insert(1,'yellow')
+print(colors)
