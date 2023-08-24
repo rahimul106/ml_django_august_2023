3087de86ae8bcb77404458caa1ca7a3322a91ed7
rahimul.islam <rahimul.islam@ibcs-primax.com>
2023-08-24 23:49:57 +0600

    Ascending/Sorting practice in python

---
+++ My Code/Day-3/ascend_try.py
@@ -0,0 +1,8 @@
+#If you want to sort the list values = [4, 1, 7, 3, 9] in ascending order, which method would you use?
+#List
+values = [4, 1, 7, 3, 9]
+print(values)
+
+#Do ascending the list
+values.sort()
+print(values)
